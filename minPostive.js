22c01ab61f49ba68be5f61fff4d1900e49c29187
Mirza Bilal <mirzabilal@Mirzas-MacBook-Pro.local>
2023-03-02 19:39:41 +0500

    min positive integer in array find

---
+++ minPostive.js
@@ -0,0 +1,39 @@
+function solution(A) {
+    // Implement your solution here
+    const maxNumber = Math.max(...A);
+    let smallestPositive = 1;
+    let found = false;
+
+    while(!found){
+        if(!A.includes(smallestPositive)) {
+        found = true;
+        }
+        
+       else if(smallestPositive >=  maxNumber){
+            found = true;
+             smallestPositive = smallestPositive + 1;
+             
+        }
+       else smallestPositive = smallestPositive + 1;
+    }
+    return smallestPositive;
+    
+}
+// second solution
+
+function solutionB(A) {
+    // Implement your solution here
+
+    const sortedArray = A.sort();
+    let num = 1;
+    let found = false;
+   for (let index = 0; index < sortedArray.length -2; index++) {
+    if( sortedArray[index+1] - sortedArray[index] > 1 ) {
+        num = sortedArray[index] + 1; 
+        found = true;
+    }
+   }
+   if(found) return num;
+   
+    
+}
